e66c47667f1aa5b1e804467c95c95ece3c35f0ef
kaixzzoo <aslamnadhif596@gmail.com>
2025-04-19 08:02:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const footer = document.querySelector('footer');
+  footer.addEventListener('click', () => {
+    alert('makasih yaa udah baca sampe akhir. kamu emang spesial bener deh!');
+  });
+});
