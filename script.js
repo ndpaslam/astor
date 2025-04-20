4677fe6ab8a41b43043b21a9cb430a8ac5ec94b2
kaixzzoo <aslamnadhif596@gmail.com>
2025-04-20 12:39:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-  const footer = document.querySelector('footer');
-  footer.addEventListener('click', () => {
-    alert('makasih yaa udah baca sampe akhir. kamu emang spesial bener deh!');
-  });
-});
